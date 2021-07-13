6c2a1490a31470d910fe4968e4c8d732f3e7594c
Heesoo Kim <heesoo0323@gmail.com>
2021-07-13 15:09:51 -0700

    listened for submut events on journal entry form and put the form's input values into a new object.

--- js/main.js
+++ js/main.js
@@ -3,8 +3,16 @@
 var $photoUrl = document.querySelector('.photoUrl');
 $photoUrl.addEventListener('input', photoHandler);
 
-var $photo = document.querySelector('img-preview');
+var $photo = document.querySelector('.img-preview');
 
 function photoHandler(event) {
   $photo.setAttribute('src', $photoUrl.value);
+}
+
+// var $entry = document.querySelector('.entry-form');
+var $button = document.querySelector('.save-button');
+$button.addEventListener('click', handleSaveButton);
+
+function handleSaveButton(event) {
+  event.preventDefault();
 }
