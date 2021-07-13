67399270714f5b6cb62ce553d7d1bb8ad67c62a1
Heesoo Kim <heesoo0323@gmail.com>
2021-07-13 16:50:24 -0700

    Listen for submit events by putting form's input values into a new object and adding the nextEntryId to the object. Incrementation on nextEntryId and prepending the new object to the entries was also done.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
 /* global data */
 /* exported data */
 var $photoUrl = document.querySelector('.photoUrl');
-$photoUrl.addEventListener('input', photoHandler);
+$photoUrl.addEventListener('input', urlHandler);
 
 var $photo = document.querySelector('.img-preview');
+$photo.addEventListener('error', errorHandler);
 
-function photoHandler(event) {
+function urlHandler(event) {
   $photo.setAttribute('src', $photoUrl.value);
 }
 
-// var $entry = document.querySelector('.entry-form');
-var $button = document.querySelector('.save-button');
-$button.addEventListener('click', handleSaveButton);
+function errorHandler(event) {
+  $photo.setAttribute('src', 'images/placeholder-image-square.jpg');
+}
+
+var $entryForm = document.querySelector('.entry-form');
+$entryForm.addEventListener('submit', handleSaveButton);
 
 function handleSaveButton(event) {
   event.preventDefault();
 }
+
+var $entryTitle = document.querySelector('#entry-title');
+var $entryUrl = document.querySelector('#entry-url');
+var $entryNotes = document.querySelector('#entry-notes');
+
+var newObject = {
+  title: $entryTitle.value,
+  url: $entryUrl.value,
+  notes: $entryNotes.value,
+  id: data.nextEntryId
+};
+
+data.nextEntryId++;
+data.entries.unshift(newObject);
