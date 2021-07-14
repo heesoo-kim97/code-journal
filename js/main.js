b70a76d71696ae7cfe331bc2b75d391dc8c69991
Heesoo Kim <heesoo0323@gmail.com>
2021-07-13 17:11:11 -0700

    checked local storage and incrementing.

--- js/main.js
+++ js/main.js
@@ -19,22 +19,22 @@ $entryForm.addEventListener('submit', handleSaveButton);
 
 function handleSaveButton(event) {
   event.preventDefault();
+
+  var $entryTitle = document.querySelector('#entry-title');
+  var $entryUrl = document.querySelector('#entry-url');
+  var $entryNotes = document.querySelector('#entry-notes');
+
+  var newObject = {
+    title: $entryTitle.value,
+    url: $entryUrl.value,
+    notes: $entryNotes.value,
+    id: data.nextEntryId
+  };
+
+  data.nextEntryId++;
+  data.entries.unshift(newObject);
+
+  $photo.setAttribute('src', 'images/placeholder-image-square.jpg');
+
+  $entryForm.reset();
 }
-
-var $entryTitle = document.querySelector('#entry-title');
-var $entryUrl = document.querySelector('#entry-url');
-var $entryNotes = document.querySelector('#entry-notes');
-
-var newObject = {
-  title: $entryTitle.value,
-  url: $entryUrl.value,
-  notes: $entryNotes.value,
-  id: data.nextEntryId
-};
-
-data.nextEntryId++;
-data.entries.unshift(newObject);
-
-$photo.setAttribute('src', 'images/placeholder-image-square.jpg');
-
-$entryForm.reset();
